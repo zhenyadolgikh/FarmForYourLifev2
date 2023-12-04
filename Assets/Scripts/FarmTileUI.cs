7f10d4a2fd57fb12f77827ce8003b14e8130b9a4
lilmspsypnts <ninashka@gmail.com>
2023-12-04 23:00:23 +0100

    FarmTile script
    PopUp() method elaborated. Check if player has enough money to afford building a farm and set farm buttons as interactable or uninteractable depending on the outcome

--- Assets/Scripts/FarmTileUI.cs
+++ Assets/Scripts/FarmTileUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,12 +8,16 @@ using UnityEngine.UI;
 public class FarmTileUI : MonoBehaviour
 {
     public GameObject optionPanel;
+    public Button[] options;
 
     public UIManager manager;
+    public int currentMoney;
 
     public int farmTileIndex = -1;
 
-    public Button[] options;
+    bool canClick = false;
+
+
 
 
     // Start is called before the first frame update
@@ -48,21 +53,54 @@ public class FarmTileUI : MonoBehaviour
         buildAction.resource = Resource.wheat;
     }
 
+    private Resource GetAssociatedResource(string buttonName)
+    {
+        // Convert button name to associated resource
+        switch (buttonName)
+        {
+            case "WheatButton":
+                return Resource.wheat;
+            case "AppleButton":
+                return Resource.apple;
+            case "CinnamonButton":
+                return Resource.cotton;
+            case "PigButton":
+                return Resource.pigMeat;
+            default:
+                // Handle unknown buttonName, you might want to throw an exception or return a default value
+                throw new ArgumentException($"Unknown buttonName: {buttonName}");
+        }
+    }
+
     private void PopUp()
     {
-        if(optionPanel != null)
+        currentMoney = manager.gameStateLogic.GetStoredResourceAmount(Resource.money);
+        Debug.Log(currentMoney);
+
+        if (optionPanel != null)
         {
             bool isActive = optionPanel.activeSelf;
             optionPanel.SetActive(!isActive);
+            
+           foreach (Button button in options)
+           {
+               Resource associateResource = GetAssociatedResource(button.name);
+               int cost = manager.gameStateLogic.GetBuildingCost(associateResource);
+               Debug.Log("Success!");
+               button.interactable = currentMoney >= cost;
 
-            options[2].enabled = false;
+                if(button.interactable)
+                {
+                    canClick = true;
+                    //manager.IsActionValid(buildAction) ??
+                }
+           }
 
             BuildAction buildAction = new BuildAction();
 
         //   if (!manager.IsActionValid(buildAction))
         //   {
-        //       optionPanel.GetComponentInChildren<FarmTile>(true);
-        //
+        //               //
         //   }
             //sätt knapparnas active utifrån IsActionValid
 

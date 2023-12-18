ca4ee30aea30fbb2c31a4a89db664fa227ddcbfe
SverkerBerggren <svartaspjutet@gmail.com>
2023-12-18 13:03:37 +0100

    Exit button functional

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -25,6 +25,12 @@ public class LevelManager : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+        #if UNITY_STANDALONE
+                Application.Quit();
+        #endif
+        
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }

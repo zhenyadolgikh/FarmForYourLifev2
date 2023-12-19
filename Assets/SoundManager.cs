422293f71b18ea4fb7a20c6acc92aae2e3f55c4d
zhenyadolgikh <apple.ijenny@gmail.com>
2023-12-19 22:06:53 +0100

    sound mute, background music

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public void Mute(bool muted) { 
+ if (muted) {
+ AudioListener.volume = 0;
+ }
+ else{
+ AudioListener.volume = 1;
+}
+}
+}

2d59e33758ec12e9750c53b767bc0eabe4d1d24f
SverkerBerggren <svartaspjutet@gmail.com>
2024-01-08 14:46:38 +0100

    Fixat referenser till scenen

--- Assets/Scripts/UI/UpgradeManager.cs
+++ Assets/Scripts/UI/UpgradeManager.cs
@@ -10,7 +10,7 @@ public class UpgradeManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("hello world");
     }
 
     // Update is called once per frame

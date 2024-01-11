5bb8da40811889ca00a137c737317dba0a846bc1
lilmspsypnts <ninashka@gmail.com>
2024-01-11 16:32:37 +0100

    Fix some stuff

--- Assets/DisableEnableButton.cs
+++ Assets/DisableEnableButton.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditor.UI;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.UIElements;
 
